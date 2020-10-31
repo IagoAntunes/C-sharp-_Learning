e92f40b24f21aae7186e150b75df1776b5e13c78
IagoAntunes <iagoantunes.f@gmail.com>
2020-10-31 01:02:33 -0300

    Iago Antunes Ferreira

---
+++ Aulas/aula06.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Aula06{
+
+    static void Main(){
+        
+        double valorCompra=5.50;
+        double valorVenda;
+        double lucro=0.1;
+        string produto="Pastel";
+
+        valorVenda = valorCompra + (valorCompra * lucro);
+
+        Console.WriteLine("Produto.......{0,15}",produto);
+        Console.WriteLine("Val.Compra....{0,15:c}",valorCompra);
+        Console.WriteLine("Lucro.........{0,15:p}",lucro);
+        Console.WriteLine("Val.Venda.....{0,15:c}",valorVenda);
+        
+
+
+
+
+        /* 
+        int n1=10,n2=20,n3=30;
+        \n  \t
+        Console.Write("n1 = {0},n2={1},n3={2}",n1,n2,n3 );
+        */
+    }
+}
---
+++ Aulas/aula07.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Aula06{
+
+    static void Main(){
+        // Atribuir tipo,valor
+
+        //Não altera-se o valor da Constante
+        const string canal=" Canal ABC";
+        const double pi=3.14;
+
+
+        Console.WriteLine("Canal{0}\nPi:{1}",canal,pi);
+
+
+
+
+
+
+
+    }
+}
