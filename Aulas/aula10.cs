0d5ab16787f6a2ad172d1e03b83f7cc31408d004
IagoAntunes <iagoantunes.f@gmail.com>
2020-11-03 02:41:33 -0300

    Iago Antunes Ferreira

---
+++ Aulas/aula10.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Aula10 {
+    enum DiasSemana{Domingo ,Segunda , Terça , Quarta , Quinta, Sexta, Sabado};
+    static void Main(){
+
+        //DiasSemana ds = DiasSemana.Domingo;
+        //DiasSemana ds = (DiasSemana)3;
+
+        int ds = (int)DiasSemana.Sexta;
+
+        Console.WriteLine(ds);
+
+
+    }
+}
---
+++ Aulas/aula11.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Aula10 {
+
+    static void Main(){
+
+        /*
+            int n1=10;
+            float n2=n1;
+            float n1=10.5f;
+            int n2=(int)n1;//type cast
+        */
+        int vInt = 10;
+        short vShort=(short)vInt;
+
+        Console.WriteLine(vShort);
+        //Console.WriteLine("N2 = "+ n2);
+
+
+    }
+}
