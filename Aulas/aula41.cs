c839b4f3a79839b72fbe34cd6cb8e31f2dfc8e79
IagoAntunes <iagoantunes.f@gmail.com>
2021-01-08 17:34:07 -0300

    Iago Antunes Ferreira

--- Aulas/aula41.cs
+++ Aulas/aula41.cs
@@ -1,8 +1,36 @@
 using System;
-//
-class Aula40{
+//GET E SET
+class Carro{
+    private int velMax;
+    public int vm{// Propriedade
+        get{
+            return velMax;
+        }
+        set{
+            if(value < 0){// Se o valor passado a Propriedade
+                velMax = 0;
+            }else if(value > 300){
+                velMax = 300;
+            }else{
+                velMax = value;
+            }
+        }
+    }
+    public Carro(){// Construtor
+        //this.velMax=120;
+        vm = 120;
+    }
+
+    /*public void vm(int velMax){
+        this.velMax = velMax;
+    }*/
+}
+class Aula41{
     static void Main(){
-        Carro c1 = new Carro();
+        Carro c1=new Carro();
+
+        c1.vm = 200;
+        Console.WriteLine("Velocidade:{0}",c1.vm);
         
     }
 }
