bf9a30281a61e80117b11ba7482cc6bdb8b0b5e4
IagoAntunes <iagoantunes.f@gmail.com>
2021-01-10 01:35:41 -0300

    Iago Antunes Ferreira

--- Aulas/aula41.cs
+++ Aulas/aula41.cs
@@ -1,24 +1,30 @@
 using System;
 //GET E SET
 class Carro{
-    private int velMax;
+    //private int velMax;
+    private int multa;
     public int vm{// Propriedade
         get{
-            return velMax;
+            return multa;
         }
         set{
             if(value < 0){// Se o valor passado a Propriedade
-                velMax = 0;
-            }else if(value > 300){
-                velMax = 300;
+                //velMax = 0;
+                multa =0;
+            }else if(value > 60){
+                //velMax = 300;
+                multa = 80;
             }else{
-                velMax = value;
+                //velMax = value;
+                multa = 0;
             }
         }
     }
     public Carro(){// Construtor
         //this.velMax=120;
-        vm = 120;
+        Console.WriteLine("Velocidade:");
+        vm = int.Parse(Console.ReadLine());
+        //vm = 120;
     }
 
     /*public void vm(int velMax){
@@ -28,9 +34,12 @@ class Carro{
 class Aula41{
     static void Main(){
         Carro c1=new Carro();
-
-        c1.vm = 200;
-        Console.WriteLine("Velocidade:{0}",c1.vm);
+        if(c1.vm == 80){
+            Console.WriteLine("Multa = {0}",c1.vm);
+        }else{
+            Console.WriteLine("Não há multas!");
+        }
+        
         
     }
 }
