42712f03daee90dbc230a0c13d7f3bce3457e255
IagoAntunes <iagoantunes.f@gmail.com>
2021-01-11 15:17:48 -0300

    Iago Antunes Ferreira

---
+++ Aulas/aula46.cs
@@ -0,0 +1,52 @@
+using System;
+// Métodos que retornam objetos
+/*
+
+*/
+
+class Galinha{
+    private string nomeGalinha;
+    private int numOvo;
+
+    public Galinha(string nomeGalinha){//Metodo(precisa dos parametros),Construtor
+        this.nomeGalinha = nomeGalinha;
+        numOvo = 0;
+    }
+    public Ovo botar(){ //Metodo para retornar objeto OVO
+        numOvo++;
+        return new Ovo(numOvo,nomeGalinha);
+        
+    }
+}
+
+class Ovo{
+    private int numOvo;
+    private string minhaGalinha;
+
+
+    public Ovo(int numOvo,string minhaGalinha){//Metodo(precisa dos parametros),Construtor
+        this.numOvo = numOvo;
+        this.minhaGalinha = minhaGalinha;
+        Console.WriteLine("-=-Ovo Criado:{0}\n{1}",this.numOvo,this.minhaGalinha);
+
+
+    }
+}
+
+class Aula46{
+    static void Main(){
+        Galinha g1 = new Galinha("Atroox");
+        Galinha g2 = new Galinha("Fizz");
+        Galinha g3 = new Galinha("Ashe");
+
+        g1.botar();
+        g1.botar();
+        g1.botar();
+
+        g2.botar();
+
+        g3.botar();
+        g3.botar();
+    }
+}
+
