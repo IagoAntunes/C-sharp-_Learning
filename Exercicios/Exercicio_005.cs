c7f3e41a28104fcb86544717aaed702d7c802f9d
IagoAntunes <iagoantunes.f@gmail.com>
2021-03-30 14:44:20 -0300

    Iago Antunes Ferreira

--- Exercicios/Exercicio_005.cs
+++ Exercicios/Exercicio_005.cs
@@ -1,17 +1,22 @@
-/*Elaborar um programa para ler a nota de 5 alunos de uma disciplina e armazenar em um vetor. Calcular e imprimir a
-média e o número de alunos que tiveram nota superior à média. Em todas as etapas do problema utilizar uma estrutura
-em laço para facilitar as tarefas.*/
+/*FATORIAL Recursivo*/
 
 using System; 
 
 
-    static void Main() { 
+    class fatorial{
 
+        static int fat(int n){
+            int resp;
+            if(n == 1){
+                resp = 1;
+            }else{
+                resp = n * fat(n - 1);
+            }
+            return resp;
+        }
+        
 
-        Console.WriteLine("")
-
-
-
-    
-
+        static void Main() { 
+            Console.WriteLine("Resposta = {0} ",fat(5));
+        }
     }
