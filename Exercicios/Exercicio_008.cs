d7fb4cfc955baf63f88b2ed00117ec964589748e
IagoAntunes <iagoantunes.f@gmail.com>
2021-04-01 03:14:05 -0300

    Iago Antunes Ferreira

---
+++ Exercicios/Exercicio_008.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int a,b,PROD;
+        
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        PROD = a * b;
+        
+        Console.WriteLine("PROD = {0}",PROD);
+
+    }
+
+}
