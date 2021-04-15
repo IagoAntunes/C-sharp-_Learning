471087e1b763941e7244c2e4320643be1544090d
IagoAntunes <iagoantunes.f@gmail.com>
2021-04-15 03:54:35 -0300

    Iago Antunes Ferreira

--- Exercicios/Exercicio_035.cs
+++ Exercicios/Exercicio_035.cs
@@ -4,24 +4,22 @@ class URI {
 
     static void Main(string[] args) { 
 
-        double A,B,C,per,area;
+        int A,B,horas=0;
 
         string[] vetor = Console.ReadLine().Split(' ');
         
-        A = double.Parse(vetor[0]);
-        B = double.Parse(vetor[1]);
-        C = double.Parse(vetor[2]);
-        
-        
-        if(A + B > C && A + C > B && B + C > A){
-            per = A + B + C;
-            Console.WriteLine("Perimetro = {0:f1}",per);
-        }else{
-            area = ((A+B)*C)/2;
-            Console.WriteLine("Area = {0:f1}",area);
+        A = int.Parse(vetor[0]);
+        B = int.Parse(vetor[1]);
+   
+        if(A > B){
+            horas = (24 - A)+B;
+        }else if(B > A ){
+            horas = (B - A);
+        }else if(A == B){
+            horas = 24;
         }
-
-
-
+        
+        Console.WriteLine("O JOGO DUROU {0} HORA(S)",horas);
+        
     }
 }
--- Exercicios/Exercicio_036.cs
+++ Exercicios/Exercicio_036.cs
@@ -4,24 +4,43 @@ class URI {
 
     static void Main(string[] args) { 
 
-        double A,B,C,per,area;
+        string opcao1,opcao2,opcao3;
 
-        string[] vetor = Console.ReadLine().Split(' ');
-        
-        A = double.Parse(vetor[0]);
-        B = double.Parse(vetor[1]);
-        C = double.Parse(vetor[2]);
-        
-        
-        if(A + B > C && A + C > B && B + C > A){
-            per = A + B + C;
-            Console.WriteLine("Perimetro = {0:f1}",per);
-        }else{
-            area = ((A+B)*C)/2;
-            Console.WriteLine("Area = {0:f1}",area);
+        opcao1 = Console.ReadLine();
+        if(opcao1 == "vertebrado"){
+            opcao2 = Console.ReadLine();
+            if(opcao2 == "ave"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "carnivoro"){
+                    Console.WriteLine("aguia");
+                }else if(opcao3 == "onivoro"){
+                    Console.WriteLine("pomva");
+                }
+            }else if(opcao2 == "mamifero"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "onivoro"){
+                    Console.WriteLine("homem");
+                }else if(opcao3 == "herbivoro"){
+                    Console.WriteLine("vaca");
+                }
+            }
+        }else if(opcao1 == "invertebrado"){
+            opcao2 = Console.ReadLine();
+            if(opcao2 == "inseto"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "hematofago"){
+                    Console.WriteLine("pulga");
+                }else if(opcao3 == "herbivoro"){
+                    Console.WriteLine("lagarta");
+                }
+            }else if(opcao2 == "anelideo"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "hematofago"){
+                    Console.WriteLine("sanguessuga");
+                }else if(opcao3 == "onivoro"){
+                    Console.WriteLine("minhoca");
+                }
+            }
         }
-
-
-
     }
 }
