2aafa005c6fd38ee1020eddc5698356fdf2b8222
IagoAntunes <iagoantunes.f@gmail.com>
2021-04-15 14:45:10 -0300

    Iago Antunes Ferreira

---
+++ Exercicios/Exercicio_037.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        for(int i=1;i<=100;i++){
+            if(i % 2 == 0){
+                Console.WriteLine("{0}",i);
+            }
+        }
+    }
+}
---
+++ Exercicios/Exercicio_039.cs
@@ -0,0 +1,47 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string opcao1,opcao2,opcao3;
+
+        opcao1 = Console.ReadLine();
+        if(opcao1 == "vertebrado"){
+            opcao2 = Console.ReadLine();
+            if(opcao2 == "ave"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "carnivoro"){
+                    Console.WriteLine("aguia");
+                }else if(opcao3 == "onivoro"){
+                    Console.WriteLine("pomba");
+                    Console.WriteLine("pomba");
+                }
+            }else if(opcao2 == "mamifero"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "onivoro"){
+                    Console.WriteLine("homem");
+                }else if(opcao3 == "herbivoro"){
+                    Console.WriteLine("vaca");
+                }
+            }
+        }else if(opcao1 == "invertebrado"){
+            opcao2 = Console.ReadLine();
+            if(opcao2 == "inseto"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "hematofago"){
+                    Console.WriteLine("pulga");
+                }else if(opcao3 == "herbivoro"){
+                    Console.WriteLine("lagarta");
+                }
+            }else if(opcao2 == "anelideo"){
+                opcao3 = Console.ReadLine();
+                if(opcao3 == "hematofago"){
+                    Console.WriteLine("sanguessuga");
+                }else if(opcao3 == "onivoro"){
+                    Console.WriteLine("minhoca");
+                }
+            }
+        }
+    }
+}
