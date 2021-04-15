b0779b84731fcff1849b910c29eac84a34da2b53
IagoAntunes <iagoantunes.f@gmail.com>
2021-04-15 15:47:31 -0300

    Iago Antunes Ferreira'

--- Exercicios/Exercicio_040.cs
+++ Exercicios/Exercicio_040.cs
@@ -1,47 +1,51 @@
 using System; 
 
 class URI {
-
+                                //7:08 9:010
     static void Main(string[] args) { 
 
-        string opcao1,opcao2,opcao3;
+        int hora=0,minuto=0,aux3,dif=0,dif2=0;
+        int initHour,initMinute,finalHour,finalMinute;
 
-        opcao1 = Console.ReadLine();
-        if(opcao1 == "vertebrado"){
-            opcao2 = Console.ReadLine();
-            if(opcao2 == "ave"){
-                opcao3 = Console.ReadLine();
-                if(opcao3 == "carnivoro"){
-                    Console.WriteLine("aguia");
-                }else if(opcao3 == "onivoro"){
-                    Console.WriteLine("pomba");
-                    Console.WriteLine("pomba");
+        string[] vetor = Console.ReadLine().Split(' ');
+        
+        initHour = int.Parse(vetor[0]);
+        initMinute = int.Parse(vetor[1]);
+        finalHour = int.Parse(vetor[2]);
+        finalMinute = int.Parse(vetor[3]);
+
+        if(initHour < 25){ 
+            if(initHour  == finalHour){
+                if(initMinute < finalMinute){
+                    hora = 0;
+                }else if(initMinute > finalMinute || initMinute == finalMinute){
+                    hora = 24;
                 }
-            }else if(opcao2 == "mamifero"){
-                opcao3 = Console.ReadLine();
-                if(opcao3 == "onivoro"){
-                    Console.WriteLine("homem");
-                }else if(opcao3 == "herbivoro"){
-                    Console.WriteLine("vaca");
-                }
+
+            }else if(initHour < finalHour){
+                hora = finalHour - initHour;
+                dif = finalHour - initHour;
+
+            }else if(initHour > finalHour){
+                aux3 = 24 - initHour;
+                hora = aux3 + finalHour;
             }
-        }else if(opcao1 == "invertebrado"){
-            opcao2 = Console.ReadLine();
-            if(opcao2 == "inseto"){
-                opcao3 = Console.ReadLine();
-                if(opcao3 == "hematofago"){
-                    Console.WriteLine("pulga");
-                }else if(opcao3 == "herbivoro"){
-                    Console.WriteLine("lagarta");
-                }
-            }else if(opcao2 == "anelideo"){
-                opcao3 = Console.ReadLine();
-                if(opcao3 == "hematofago"){
-                    Console.WriteLine("sanguessuga");
-                }else if(opcao3 == "onivoro"){
-                    Console.WriteLine("minhoca");
+            if(initMinute == finalMinute){
+                minuto = initMinute - finalMinute;
+            }
+            else if(finalMinute > initMinute){
+                minuto = finalMinute - initMinute;
+                
+            }else if(initMinute > finalMinute){
+                dif2 = initMinute - finalMinute;
+                minuto = 60 - dif2;
+                if(hora == 0){
+                    hora = 0;
+                }else{
+                    hora = hora - 1;
                 }
             }
         }
+        Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)",hora,minuto);
     }
 }
