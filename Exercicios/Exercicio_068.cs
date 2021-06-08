d3214e8381680a67892b6384896c69344b49f89e
Iago Antunes <iagoantunes.f@gmail.com>
2021-06-08 16:00:58 -0300

    Iago Antunes Ferreira

--- Exercicios/Exercicio_068.cs
+++ Exercicios/Exercicio_068.cs
@@ -3,13 +3,26 @@ using System;
 class URI {
     // URI 1155
     static void Main() { 
-        float num = 1;
-        float soma=0;
-        for(int i =1;i<=100;i++){
+        
+        int N;
+        int j=0,soma=0;
 
-            soma = soma + (num/i);
-
+        N = int.Parse(Console.ReadLine());
+        while(N!=0){
+            string[] vetor = Console.ReadLine().Split(' ');
+        
+            int X = int.Parse(vetor[0]);
+            int Y = int.Parse(vetor[1]);
+            j = 0;
+            soma = 0;
+            for(int i = X;j<Y;i++){
+                if(i % 2 != 0){
+                    soma = soma + i;
+                    j++;
+                }
+            }
+            N--;
+            Console.WriteLine("{0}",soma);
         }
-        Console.WriteLine("{0:f2}",soma);
     }
 }
