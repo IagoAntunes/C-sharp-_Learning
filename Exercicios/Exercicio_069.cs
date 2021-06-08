dc6aa2ef0ac056b52ca45fe4333e198d7e9adc0c
Iago Antunes <iagoantunes.f@gmail.com>
2021-06-08 16:15:07 -0300

    Iago Antunes Ferreira

--- Exercicios/Exercicio_069.cs
+++ Exercicios/Exercicio_069.cs
@@ -5,19 +5,21 @@ class URI {
     //1159
     static void Main() { 
 
-       int X,soma;
-
-       while(X != 0){
-           X = int.Parse(Console.ReadLine());
-           for(int i = X;j<5;i++){
-               if(i % 2 == 0){
+        int X=1,soma;
+        int j;
+        X = int.Parse(Console.ReadLine());
+        while(X != 0){
+    
+            j =0;
+            soma = 0;
+            for(int i = X;j<5;i++){
+                if(i % 2 == 0){
                    j++;
                    soma = soma + i;
                }
-           }
+            }
             Console.WriteLine("{0}",soma);
-
-       }
-        
+            X = int.Parse(Console.ReadLine());
+       } 
     }
 }
