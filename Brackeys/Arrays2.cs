9cb16bf5306fd851b1dd2801ea5171bf293ae460
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-23 04:35:11 -0300

    Iago Antunes Ferreira

--- Brackeys/Arrays2.cs
+++ Brackeys/Arrays2.cs
@@ -1,14 +1,29 @@
-using System; 
+using System;
+using System.Collections.Generic;
 
 class minhaClasse{
 
     static void Main() { 
 
-       
+       List<string> shoppingList = new List<string>();
+
+       shoppingList.Add("Dreams");
+       shoppingList.Add("Miracles");
+       shoppingList.Add("Rainbows");
+       shoppingList.Add("Pony");
+
+       for(int i = 0;i<shoppingList.Count;i++){
+           Console.WriteLine(shoppingList[i]);
+       }
     
+        shoppingList.Remove("Dreans");
+        shoppingList.RemoveAt(1);
 
+        Console.WriteLine("--------");
 
-
+        for(int i =0;i<shoppingList.Count;i++){
+            Console.WriteLine(shoppingList[i]);
+        }
 
 
     }
