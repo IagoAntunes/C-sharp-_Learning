123ba79f97fc73468bad67fc984af28042ce00a4
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-23 03:17:27 -0300

    Iago Antunes Ferreira

---
+++ Brackeys/Loops.cs
@@ -0,0 +1,20 @@
+
+using System; 
+
+class minhaClasse{
+
+    static void Main() { 
+
+       Console.WriteLine("How many cool numbers do you want");
+
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        for(int i= 1;i<= count;i++){
+            double result = Math.Pow(2,1);
+            Console.WriteLine(result);
+        }
+
+
+    }
+
+}
