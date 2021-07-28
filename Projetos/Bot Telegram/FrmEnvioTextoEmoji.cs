419dca5686c93f55f2bb154aa3efa29b164d8545
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-27 23:16:28 -0300

    Iago Antunes Ferreira

---
+++ Projetos/Bot Telegram/FrmEnvioTextoEmoji.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+
+namespace BotTelegram1
+{
+    public partial class FrmEnvioTextoEmoji : Form
+    {
+        public FrmEnvioTextoEmoji()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_ClickAsync(object sender, EventArgs e)
+        {
+            if (txtMensagem.Text.Trim().Equals(string.Empty))
+            {
+                MessageBox.Show("Informe a mensagem para envio", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            try
+            {
+                var telegramBot = new TelegramBotClient("1755387732:AAHWVTH6AeabrHiEA2R73OUK52oN6gZw9i0");
+                var strMensagem = txtMensagem.Text.Trim();
+
+                if (strMensagem.Contains(":white_check_mark:")){
+                    strMensagem = strMensagem.Replace(":white_check_mark:",char.ConvertFromUtf32(0x2705));
+                }
+                if (strMensagem.Contains(":grin:"))
+                {
+                    strMensagem = strMensagem.Replace(":grin:", char.ConvertFromUtf32(0x1F601));
+                }
+
+                
+                await telegramBot.SendTextMessageAsync(chatId: "-1001506779457", text: strMensagem, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+                MessageBox.Show("Mensagem enviada com sucesso", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Erro ao enviar a mensagem!!"+ex.Message,this.Text,MessageBoxButtons.OK,MessageBoxIcon.Error);
+            
+            }
+            finally
+            {
+                txtMensagem.Text = string.Empty;
+            }
+
+
+        }
+    }
+}
