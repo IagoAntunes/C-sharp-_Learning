0375485f95e3c8894c3381cc815aaaea73118fe9
Iago Antunes <iagoantunes.f@gmail.com>
2021-08-11 16:54:33 -0300

    Iago Antunes Ferreira

--- Projetos/FlappyBird/Program.cs
+++ Projetos/FlappyBird/Program.cs
@@ -1,22 +1,82 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AngryBirds
+namespace JogoDinossauro
 {
-    static class Program
+
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
-        [STAThread]
-        static void Main()
+
+        //Velocidade dos obstaculos
+        int Velocidade = 10;
+        //Posição
+        int pulo = 115;
+      
+
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Iniciar());
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && pulo > 55 && pulo < 175)
+            {
+                pulo = pulo - 60;
+                dinossauro.Left += 20;
+
+            }
+        }
+      
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            pulo = pulo + 60;
+            dinossauro.Left -= 10;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dinossauro.Top = pulo;
+            cacto2.Left -= Velocidade;
+            cacto1.Left -= Velocidade;
+            //Dinossauro no 100
+            if(cacto1.Left <= -20)
+            {
+                cacto1.Left = 850;
+            }
+            if(cacto2.Left <= -20)
+            {
+                cacto2.Left = 850;
+            }
+
+            if (dinossauro.Bounds.IntersectsWith(cacto1.Bounds) || 
+                dinossauro.Bounds.IntersectsWith(cacto2.Bounds)) 
+            {
+                fim();
+            }
+        }
+
+
+        private void fim()
+        {
+            timer1.Stop();
+            btnRestart.Visible = true;
+            lblRestart.Visible = true;
+
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+
+
         }
     }
 }
