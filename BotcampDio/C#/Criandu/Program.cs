805ad7476b3d7423ec4319da0d1a53961de9787a
Iago Antunes <iagoantunes.f@gmail.com>
2021-08-14 04:34:08 -0300

    Iago Antunes Ferreira

--- BotcampDio/C#/Criandu/Program.cs
+++ BotcampDio/C#/Criandu/Program.cs
@@ -1,17 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Criandu
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica,0,0,"Eliezaer zarpelao");
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-
-
-            Console.WriteLine(minhaConta.ToString());
+            while(opcaoUsuario.ToUpper() != "X"){
+                switch(opcaoUsuario){
+                    case "1":
+                        //ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                        //Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utiliar nossos serviços!!");
+            Console.WriteLine();
         }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
+            
+            Console.Write("Digite 1 para Conta Fisica ou 2 para Juridica");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Nome do Cliente:");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSado = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o credito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                        saldo:entradaSado,
+                                        credito:entradaCredito,
+                                        nome:entradaNome);
+
+            listContas.Add(novaConta);
+        }
+
+        private static string ObterOpcaoUsuario(){
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank a seu disporr!!");
+            Console.WriteLine("Informe a opção desejada!!");
+
+            Console.WriteLine("1. Listar Contas!");
+            Console.WriteLine("2. Inserir nova conta");
+            Console.WriteLine("3. Transferir");
+            Console.WriteLine("4. Sacar");
+            Console.WriteLine("5. Depositar");
+            Console.WriteLine("6. Limpar Tela");
+            Console.WriteLine("X. Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToString();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+
 
 
     
