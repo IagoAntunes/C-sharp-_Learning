dc8658571204564cc887dfd952264d312f84de95
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-09 14:47:41 -0300

    Iago Antunes Ferreira

---
+++ POO/07Construtor/Pessoa.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+
+class Pessoa{
+
+    public Pessoa(){
+        Console.WriteLien("Construtor iniciado");
+    }
+    public Pessoa(String nome){
+        Console.WriteLine("Ola + " + nome);
+    }
+
+
+
+
+
+
+}
---
+++ POO/07Construtor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _07Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            
+        }
+    }
+}
