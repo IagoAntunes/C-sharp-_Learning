807db50caff735054e876f4516914bed7e920712
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-10 16:45:01 -0300

    Iago Antunes Ferreira

---
+++ POO/12Estatico/Exemplo.cs
@@ -0,0 +1,21 @@
+using System;
+
+
+
+class Exemplo{
+
+
+    public static string Nome = "luca";
+
+    public static void soma(int n1,int n2){
+        Console.WriteLine(n1 + n2);
+    }
+
+
+
+
+    
+
+}
+
+
---
+++ POO/12Estatico/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12Estatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo.soma(3,7);
+            
+            Console.WriteLine("oLA +"+Exemplo.Nome);
+
+
+
+
+
+        }
+    }
+}
