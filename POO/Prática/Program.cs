3a1e6951cf09ee38b513a8faa5b866d90779a490
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-12 23:36:58 -0300

    Iago Antunes Ferreira

--- POO/Prática/Program.cs
+++ POO/Prática/Program.cs
@@ -9,7 +9,6 @@ namespace Prática
             Trabalhador trab = new Trabalhador(1,"Lucas",1100);
 
             trab.gstotalProdutos = 0;
-
             Console.WriteLine("Nome:{0}\nSalário:{1}\nMatricula:{2}",trab.Nome,trab.Salario,trab.Matricula);
             trab.aumento();
             Console.WriteLine("Nome:{0}\nSalário:{1}\nMatricula:{2}",trab.Nome,trab.Salario,trab.Matricula);
--- POO/Prática/Trabalhador.cs
+++ POO/Prática/Trabalhador.cs
@@ -1,9 +1,9 @@
 using System;
 
 class Trabalhador : Empresa{
-    private int matricula;
-    private string nome;
-    private double salario;
+    public int matricula{get;set;}
+    public string nome{get;set;}
+    public double salario{get;set;}
 
 
     public Trabalhador(int matricula,string nome,double salario){
@@ -11,23 +11,10 @@ class Trabalhador : Empresa{
         this.nome = nome;
         this.salario = salario;
     }
-    public double Salario{
-        get{return salario;}
-        set{salario = value;}
-    }
-    public string Nome{
-        get{return nome;}
-        set{nome = value;}
-    }
-    public int Matricula{
-        get{return matricula;}
-        set{matricula = value;}
-    }
     public void aumento(){
         if(gstotalProdutos > 50){
             Salario = ((Salario * 10)/100) + Salario;
         }
-        
     }
 
 
